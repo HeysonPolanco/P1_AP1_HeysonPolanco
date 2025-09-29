b5b1097fc4f389432f94484b058bd0235c8266b0
HeysonLaptop <Heysonpolanco65@gmail.com>
2025-09-29 18:17:15 -0400

    Se cambio a entrada de guacales

---
+++ P1_AP1_HeysonPolanco/Models/Entradas_Guacales.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Register.Models;
+
+public class Entradas_Guacales
+
+{
+	[Key]
+
+	public int Id { get; set; }
+
+	[Required(ErrorMessage = "This is Requiered")]
+
+	public string Context { get; set; } = null!;
+
+}
