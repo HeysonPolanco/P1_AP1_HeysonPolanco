99cac590801da28be8745a78c1e8d5d41d87d9a4
HeysonLaptop <Heysonpolanco65@gmail.com>
2025-10-05 11:32:55 -0400

    added DAL, Models, Service added Sql Server connexion

--- P1_AP1_HeysonPolanco/Program.cs
+++ P1_AP1_HeysonPolanco/Program.cs
@@ -1,26 +1,29 @@
 using P1_AP1_HeysonPolanco.Components;
+using Microsoft.EntityFrameworkCore;
+using P1_AP1_HeysonPolanco.DAL;
+using P1_AP1_HeysonPolanco.Models;
+using P1_AP1_HeysonPolanco.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var ConStr = builder.Configuration.GetConnectionString("SqlConnection");
+builder.Services.AddDbContextFactory<Context>(o => o.UseSqlite(ConStr));
+
+builder.Services.AddScoped<EntradasGuacalesService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
