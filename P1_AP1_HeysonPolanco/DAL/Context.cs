1288e5b1e426572afb9029b1b6882783386ab820
HeysonLaptop <Heysonpolanco65@gmail.com>
2025-10-05 11:30:10 -0400

    added Context.cs

---
+++ P1_AP1_HeysonPolanco/DAL/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_HeysonPolanco.Models;
+
+namespace P1_AP1_HeysonPolanco.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<EntradasGuacales> EntradasGuacales { get; set; }
+    }
+}
