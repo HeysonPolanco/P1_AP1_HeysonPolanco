ef3a1dd4e1ddea9eeaaa25a1bdd944d0a3a142e2
HeysonLaptop <Heysonpolanco65@gmail.com>
2025-10-05 11:30:46 -0400

    added EntradasGuacales

---
+++ P1_AP1_HeysonPolanco/Models/EntradasGuacales.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_HeysonPolanco.Models
+{
+    public class EntradasGuacales
+    {
+        [Key]
+        public int IdEntrada { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required, StringLength(100)]
+        public string NombreCliente { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public decimal Precio { get; set; }
+    }
+}
